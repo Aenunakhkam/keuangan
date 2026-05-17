c8878a99ea31cdee6a50f69daea4e2842ade2e88
Aenun Akhkam <aenunakhkam@gmail.com>
2026-05-17 17:18:29 +0700

    feat: membuat favicon dinamis mengikuti logo sekolah yang diunggah di pengaturan umum

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,21 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        @php
+            $favicon = asset('favicon.ico');
+            try {
+                $dbFavicon = \App\Models\Setting::where('key', 'logo_url')->value('value') 
+                    ?? \App\Models\Setting::where('key', 'school_logo')->value('value');
+                if ($dbFavicon) {
+                    $favicon = $dbFavicon;
+                }
+            } catch (\Exception $e) {
+                // Fail silently
+            }
+        @endphp
+        <link rel="icon" href="{{ $favicon }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
